34073b980ea965df7215fb845a08139b2cda2a0b
frank <francisco.graya26@gmail.com>
2022-04-08 11:26:56 -0500

    Se subio transporte UNA ANOTACION

--- src/main/java/com/avalon/Avalon/Repository/TransporteRepository.java
+++ src/main/java/com/avalon/Avalon/Repository/TransporteRepository.java
@@ -2,6 +2,8 @@ package com.avalon.Avalon.Repository;
 
 import com.avalon.Avalon.Model.Transporte;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface TransporteRepository extends JpaRepository<Transporte, Long> {
 }
